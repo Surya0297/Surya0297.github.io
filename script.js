cd7229c8150412163f05d2fef39d81903de21797
surya chauhan <chauhansurya3022gmail.com>
2023-05-10 02:05:45 +0530

    final changes

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ GitHubCalendar(".calendar", "Surya0297", {
     global_stats: false,
     tooltips: true,
 });
+
+const hamburger = document.getElementById("hamburger");
+const menulist = document.getElementById("menu-list");
+
+hamburger.addEventListener('click', () => {
+
+
+    menulist.classList.toggle('show');
+});
